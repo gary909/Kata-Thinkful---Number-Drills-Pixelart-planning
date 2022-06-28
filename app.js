ed0c8d5e8649a06b5c275b0da5949ab4826647ee
gary <gary.white@lodgit.com>
2022-06-28 13:36:16 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function isDivisible(wallLength, pixelSize) {
+    //your code here
+}
+
+console.log(isDivisible(4050, 27)); // true
+console.log(isDivisible(4066, 27)); // false
+console.log(isDivisible(10000, 20)); // true
+console.log(isDivisible(10005, 20)); // false
+console.log(isDivisible(10005, 1)); // true
